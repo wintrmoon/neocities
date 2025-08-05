9c2f04f8db23909ee2d4e7f83f9270e7db007918
wintrmoon <ar9090games+1@gmail.com>
2025-08-05 12:57:11 +0200

    fixed archive
    this fixes https://github.com/wintrmoon/neocities/issues/29
    a bit sloppy but it works

--- public/archive/templates.js
+++ public/archive/templates.js
@@ -1,4 +1,5 @@
 //ANCHOR - Side bar template
+//TODO - add more or idk
 class NavBoxSide extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
@@ -7,18 +8,18 @@ class NavBoxSide extends HTMLElement {
           navigation
         </h2>
         <ul class="nav-list">
-          <li><a href="./index.html"><img src="./assets/icons/mono-house.png" class="icon"> home</a></li>
-          <li><a href="./not_found.html"><img src="./assets/icons/mono-book.png" class="icon"> journal</a></li>
-          <li><a href="./creations.html"><img src="./assets/icons/mono-mail.png" class="icon"> my creations</a></li>
-          <li><a href="./links.html"><img src="./assets/icons/mono-star.png" class="icon"> links</a></li>
-          <li><a href="./about.html"><img src="./assets/icons/mono-pen.png" class="icon"> about</a></li>
+          <li><a href="./index.html"><img src="../assets/icons/mono-house.png" class="icon"> home</a></li>
+          <li><a href="./not_found.html"><img src="../assets/icons/mono-book.png" class="icon"> journal</a></li> <!--FIXME - add new page when done -->
+          <li><a href="./creations.html"><img src="../assets/icons/mono-mail.png" class="icon"> my creations</a></li>
+          <li><a href="./links.html"><img src="../assets/icons/mono-star.png" class="icon"> links</a></li>
+          <li><a href="./about.html"><img src="../assets/icons/mono-pen.png" class="icon"> about</a></li>
         </ul>
       </nav>
       <br>
-      <div class="divider"><img src="./assets/divider_half.gif"></div>
+      <div class="divider"><img src="../assets/divider_half.gif"></div>
       <div style="text-align: center;">
         <p>my button!</p>
-        <img src="./assets/buttons/wintr button ani.gif" alt="website button">
+        <img src="../assets/buttons/wintr button ani.gif" alt="website button">
         <!-- <p>webrings</p> -->
         <div>
             <!--NOTE - the last fm thingy... Hi harv, thanks for the help! I was pulling my hair out-->
@@ -55,18 +56,17 @@ class NavBoxSide extends HTMLElement {
 
 class Footer extends HTMLElement {
   connectedCallback() {
-    //TODO - needs more buttons
     this.innerHTML = ` 
     <footer>
       <div class="footer-row">
-        <img src="./assets/buttons/eevee.png" alt="eevee" />
-        <img src="./assets/buttons/tyg.gif" alt="tyg" />
+        <img src="../assets/buttons/eevee.png" alt="eevee" />
+        <img src="../assets/buttons/tyg.gif" alt="tyg" />
         <p>est. 11*06*'24 | made by wintrmoon</p>
-        <img src="./assets/buttons/hiro88.gif" alt="hiro88" />
-        <img src="./assets/buttons/neocities_hosted.gif" alt="hosted" />
+        <img src="../assets/buttons/hiro88.gif" alt="hiro88" />
+        <img src="../assets/buttons/neocities_hosted.gif" alt="hosted" />
       </div>
       <div>
-        <img src="./assets/zipcat.gif" style="margin-bottom: -10px" />
+        <img src="../assets/zipcat.gif" style="margin-bottom: -10px" />
       </div>
     </footer>
         `;
