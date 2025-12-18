7635adb473c69601f2b1343eaf587195602e29ef
wintrmoon <217111326+wintrmoon@users.noreply.github.com>
2025-12-18 17:13:58 +0100

    ugly but it works
    I'm talking about the text area,,, not me

--- public/templates.js
+++ public/templates.js
@@ -19,7 +19,7 @@ class NavBoxSide extends HTMLElement {
       <div style="text-align: center;">
         <p>my button!</p>
         <img src="./assets/buttons/peachleaf.gif" alt="website button" data-tooltip="a button >:3">
-        <textarea rows="2" style="resize: none;"><a target="_blank" href="https://peachleaf.neocities.org/"><img src="https://peachleaf.neocities.org/assets/buttons/peachleaf.gif" alt="peachleaf's button"/></a></textarea>
+        <textarea rows="3" style="resize: none;"><a target="_blank" href="https://peachleaf.neocities.org/"><img src="https://peachleaf.neocities.org/assets/buttons/peachleaf.gif" alt="peachleaf's button"/></a></textarea>
         <div>
             <!--NOTE - the last fm thingy... Hi harv, thanks for the help! I was pulling my hair out-->
           <p>listening to:<br> <iframe src="https://petrapixel.neocities.org/widgets/lastfm?center=1&marquee=0&font=Courier New&fontSize=16px&color=FCEAC5&username=Oatax&swapPositions=0&delimiter=by&underline=0" width="170" frameborder="0" title="Last.Fm Status"></iframe></p>
